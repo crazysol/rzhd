fd1bb05d19932f7537cc48dc08c30d94fd7f7c0c
CrSolutions <cherya.chernikov@gmail.com>
2013-06-15 16:49:58 +0300

    First Test Was added

---
+++ src/test/java/rzhdpack/OpenRzd.java
@@ -0,0 +1,24 @@
+package rzhdpack;
+
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+import static org.testng.AssertJUnit.*;
+
+public class OpenRzd {
+
+	@Test
+	public void openRzdRuAndVerifyItIsOpened() throws InterruptedException {
+		WebDriver driver = new FirefoxDriver();
+		driver.get("http://rzd.ru/");
+		List<WebElement> rzdLink = driver.findElements(By.xpath(
+				"//a[@href='http://rzd.ru']"));
+		assertEquals(1, rzdLink.size());
+		driver.close();
+	}
+
+}
