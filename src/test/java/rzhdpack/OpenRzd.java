5ded27a5b1d6e10a88572420327f320c17120051
crazysol <crazy_solutions@engineer.com>
2013-06-16 15:33:11 +0300

    add some checks

--- src/test/java/rzhdpack/OpenRzd.java
+++ src/test/java/rzhdpack/OpenRzd.java
@@ -1,5 +1,6 @@
 package rzhdpack;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.openqa.selenium.By;
@@ -10,49 +11,75 @@ import org.testng.annotations.AfterTest;
 import org.testng.annotations.Test;
 import static org.testng.AssertJUnit.*;
 
-
 public class OpenRzd {
-
+	
 	public static WebDriver driver = new FirefoxDriver();
-		
+
 	@Test
 	public void openRzdRuAndVerifyItIsOpened() throws InterruptedException {
+		Common common = new Common();
 		driver.get("http://rzd.ru/");
 		// List<WebElement> rzdLink = driver.findElements(By.xpath(
-			//	"//a[@href='http://rzd.ru']"));
+		// "//a[@href='http://rzd.ru']"));
 		// assertEquals("Link wasn't found", 0, rzdLink.size());
-		
+
 		driver.findElement(By.cssSelector("#name0")).sendKeys("Москва");
 		driver.findElement(By.cssSelector("#name1")).sendKeys("Харьков");
 		driver.findElement(By.cssSelector("#date0")).clear();
-		
-		driver.findElement(By.cssSelector("#date0")).sendKeys("30.06.2013");
+
+		driver.findElement(By.cssSelector("#date0")).sendKeys("03.07.2013");
 		driver.findElement(By.cssSelector("#Submit")).click();
+
+		common.waitForElementPresent("//div[@id='Part0']//input[@name='car-type4']");
+
+		 driver.findElement(By.xpath("//div[@id='Part0']//input[@name='car-type4']")).click();
+		 driver.findElement(By.xpath("//div[@id='Part0']//input[@name='car-type5']")).click();
+		 driver.findElement(By.xpath("//div[@id='Part0']//input[@name='car-type6']")).click();
+
+		driver.findElement(
+				By.xpath("//div[@id='Part0']//button[contains(@class,'btn-color-grey btn-icon')]"))
+				.click();
+
+		List<WebElement> trains = driver
+				.findElements(By
+						.xpath("//div[@class='pass_IU_TrainChoice__trainList']//div[@class='trslot trainBlock']"));
 		
-		Common common = new Common();
 		
-		common.waitForAjax();
-		// Thread.sleep(10000);
-		
-		driver.findElement(By.xpath("//div[@id='Part0']//input[@name='car-type4']")).click();
-		driver.findElement(By.xpath("//div[@id='Part0']//input[@name='car-type5']")).click();
-		driver.findElement(By.xpath("//div[@id='Part0']//input[@name='car-type6']")).click();
-		
-		driver.findElement(By.xpath("//div[@id='Part0']//button[contains(@class,'btn-color-grey btn-icon')]")).click();
-		
-		//common.waitForAjax();
+		for (WebElement train:trains)
+		{
+			String number = train.findElement(By.xpath(".//td[@class='pass_trListCol_2']//span[@class='train-num-0']")).getText();
+			String from = train.findElement(By.xpath(".//td[@class='pass_trListCol_3']//span[@class='train-time']")).getText() +
+					" " + train.findElement(By.xpath(".//td[@class='pass_trListCol_3']//b[@class='train-date']")).getText();
+			
+			String to = train.findElement(By.xpath(".//td[@class='pass_trListCol_7']//span[@class='train-time']")).getText() +
+					" " + train.findElement(By.xpath(".//td[@class='pass_trListCol_7']//b[@class='train-date']")).getText();
+			
+			String seats = train.findElement(By.xpath(".//td[@class='pass_trListCol_8 free-seats']//b")).getText();
+			
+			String cost = train.findElement(By.xpath(".//td[@class='pass_trListCol_8 free-seats']//span")).getText();
+			
+			List<List<String>> trains_table = new ArrayList <List<String>> ();
+			
+			List<String> train_row = new ArrayList<String>();
+			train_row.add(number);
+			train_row.add(from);
+			train_row.add(to);
+			train_row.add(seats);
+			train_row.add(cost);
+			trains_table.add(train_row);
+			System.out.println(trains_table);
+			
+			
+			Thread.sleep(1000);
+		}
+		// common.waitForAjax();
 		Thread.sleep(10000);
-		
-		
-		
-		
-		
+
 	}
-	
+
 	@AfterTest
-	public void closeBrowser(){
+	public void closeBrowser() {
 		driver.close();
 	}
-	
 
 }
