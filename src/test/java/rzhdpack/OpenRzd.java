84450b0c5075b379746d1e0d924ac3a794fc6970
crazysol <crazy_solutions@engineer.com>
2013-06-16 12:29:15 +0300

    main logic

--- src/test/java/rzhdpack/OpenRzd.java
+++ src/test/java/rzhdpack/OpenRzd.java
@@ -18,9 +18,29 @@ public class OpenRzd {
 	@Test
 	public void openRzdRuAndVerifyItIsOpened() throws InterruptedException {
 		driver.get("http://rzd.ru/");
-		List<WebElement> rzdLink = driver.findElements(By.xpath(
-				"//a[@href='http://rzd.ru']"));
-		assertEquals("Link wasn't found", 0, rzdLink.size());
+		// List<WebElement> rzdLink = driver.findElements(By.xpath(
+			//	"//a[@href='http://rzd.ru']"));
+		// assertEquals("Link wasn't found", 0, rzdLink.size());
+		
+		driver.findElement(By.cssSelector("#name0")).sendKeys("Москва");
+		driver.findElement(By.cssSelector("#name1")).sendKeys("Харьков");
+		driver.findElement(By.cssSelector("#date0")).clear();
+		
+		driver.findElement(By.cssSelector("#date0")).sendKeys("30.06.2013");
+		driver.findElement(By.cssSelector("#Submit")).click();
+		Thread.sleep(10000);
+		
+		driver.findElement(By.xpath("//div[@id='Part0']//input[@name='car-type4']")).click();
+		driver.findElement(By.xpath("//div[@id='Part0']//input[@name='car-type5']")).click();
+		driver.findElement(By.xpath("//div[@id='Part0']//input[@name='car-type6']")).click();
+		
+		driver.findElement(By.xpath("//div[@id='Part0']//button[contains(@class,'btn-color-grey btn-icon')]")).click();
+		
+		Thread.sleep(10000);
+		
+		
+		
+		
 		
 	}
 	
